97c01f79060fd1cf8ce0018f2c005493a738e1a3
abhikr1871 <abhikr1871@gmail.com>
2025-06-26 18:23:29 +0530

    minor

--- my-app/src/environment.js
+++ my-app/src/environment.js
@@ -1,3 +1,3 @@
 export const API_URL = "https://college-cart-backend.onrender.com/api"
 export const SOCKET_URL = "https://college-cart-backend.onrender.com/"
-export const FRONTEND_URL = "http://localhost:3000"
+export const FRONTEND_URL = "https://college-cart.onrender.com/"
