94ca512143640a863f4ac6f22aa8f64bb35e2f15
abhikr1871 <abhikr1871@gmail.com>
2026-01-15 22:35:54 +0530

    production ready

--- my-app/src/environment.js
+++ my-app/src/environment.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:4000/api"
-export const SOCKET_URL = "http://localhost:4000/"
-export const FRONTEND_URL = "http://localhost:3000/"
+export const API_URL = process.env.REACT_APP_API_URL || "http://localhost:4000/api"
+export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL || "http://localhost:4000/"
+export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:3000/"
